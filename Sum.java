02d6d37d30c5f2fc4be9d2feb174f7f6df1ca70f
Navyasrigayatri <115761032+Navyasrigayatri@users.noreply.github.com>
2022-10-14 09:16:00 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        int n=5,i,sum=0;
+        for(i=0;i<=n;i++)
+        sum=sum+i;
+        System.out.println("sum ="+sum);
+    }
+    
+}
